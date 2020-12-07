8297b48b6f90c683d4a1dd6bcdec4df98df0a3ee
xTOMATOx <xTOMATOx@users.noreply.github.com>
2020-12-07 12:05:19 +0800

    Update create_department.php

--- Public/View/Admin/create_department.php
+++ Public/View/Admin/create_department.php
@@ -6,24 +6,73 @@
  $dept = new Department();
 
  $dept_obj = $dept->read('department', 'department', array('department_status','=','Active'));
- 
+
 ?>
 
 
-
-
+<link href="css/simple-sidebar.css" rel="stylesheet">
 <?php include_once '../../../Public/layouts/header.php'; ?>
 
 <section id="cover">
+   
+    <div class ="container"> 
+
+        <!-- row -->
+        <div class="row">
+
+            <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto form p-4 border border-secondary">
+            
+            <p class="h4 mb-4 text-center border-bottom">Active Departments</p>
+            
+            <?php if(empty($dept_obj)): ?>
+                <div class="row border-bottom border-secondary justify-content-center p-1">No Departments Yet</div>
+            <?php endif; ?>
+
+            <?php foreach($dept_obj as $obj => $value): ?>
+                
+                <div class="row border-bottom border-secondary justify-content-center p-1"><?php echo $value; ?></div>
+
+            <?php endforeach; ?>
+                <div class="row border-bottom border-secondary justify-content-center p-1"><a href="#" onClick="$('#addDepartmentModal').modal('show');">Add Department</a></div>
+            </div>
+         
+        </div> <!-- row -->
+
+    </div><!-- container -->
 
 
-    <div class="modal fade" id="alertModal" role="dialog">
+<!-- Modals -->
+
+<div class="modal fade" id="alertModal" role="dialog"><!--Alert Modal-->
         <div class="modal-dialog">
-    
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div id="message"class="modal-body bg-success text-white font-weight-bold">
+                </div>
+                <div class="modal-footer bg-success">
+                <button type="button" class="btn btn-default text-white" data-dismiss="modal" onclick="location.reload()">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div><!--Alert Modal-->
+
+    <div class="modal fade" id="addDepartmentModal" role="dialog"><!--Add Department Modal-->
+        <div class="modal-dialog">
+
             <!-- Modal content-->
             <div class="modal-content">
                 <div class="modal-body">
-                <p id="message"></p>
+                    <!-- Form Add Department -->
+                    <form id="frm" class="justify-content-center border border-secondary p-5 mt-5 form-color" action="<?php echo $_SERVER['PHP_SELF'] ?>" method ="POST">
+                        <p class="h4 mb-4 text-center">Add Department</p>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="dept_name" placeholder="Department Name" autocomplete="off">
+                            <span class="invalid-feedback" ></span>
+                        </div>
+                        <input type="submit" class="btn btn-info  btn-block" name="btn_add" value="Submit" onClick="$('#addDepartmentModal').modal('hide');">
+                    </form><!--Form Add Department -->
                 </div>
                 <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -31,48 +80,9 @@
             </div>
 
         </div>
-    </div>
-   
-    <div class ="container"> 
+    </div><!--Add Department Modal-->
 
-        <!-- row -->
-        <div class="row ">
-            <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto form p-4">
-                
-            <?php foreach($dept_obj as $obj): ?>
-                
-                    <?php foreach($obj as $arr => $value): ?>
-                    
-                        <div class="row-lg border border-secondary justify-content-center p-1"><?php echo $value ?? ''; ?></div>
 
-                    <?php endforeach;?>
-                
-            <?php endforeach; ?>
-            </div>
-         
-        </div> <!-- row -->
-
-        <!-- row -->
-        <div class="row ">
-            <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto form p-4">
-                <!-- form Add Department -->
-                <form id="frm" class="justify-content-center border border-secondary p-5 mt-5 form-color" action="<?php echo $_SERVER['PHP_SELF'] ?>" method ="POST">
-
-                    <p class="h4 mb-4 text-center">Add Department</p>
-
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="dept_name" placeholder="Department Name" autocomplete="off">
-                            <span class="invalid-feedback" ></span>
-                        </div>
-
-                    <input type="submit" class="btn btn-info  btn-block" name="btn_add" value="Submit">
-
-                </form><!-- Add Department -->
-                
-            </div>
-         
-        </div> <!-- row -->
-    </div><!-- container -->
 
 <?php include_once '../../../Public/layouts/footer.php'; ?>
 <script>
@@ -85,9 +95,9 @@ $('form').submit(function(event) {
     $.ajax({
         type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
         url         : 'Process_Department.php', // the url where we want to POST
-        dataType    : 'json',
+        dataType    : 'json', // define data type
         data        : $("#frm").serialize(), // our data object
-        success     : function(data)
+        success     : function(data) //execute this function if success
         {
             console.log(data);
             $("#message").html(data.message);
@@ -99,4 +109,5 @@ $('form').submit(function(event) {
 
 });
 </script>
-  
+
+
