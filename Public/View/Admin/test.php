5fecf5667be277f216dcdefc9470a17718a26a4a
xTOMATOx <xTOMATOx@users.noreply.github.com>
2020-12-09 22:44:10 +0800

    Update test.php

--- Public/View/Admin/test.php
+++ Public/View/Admin/test.php
@@ -13,5 +13,5 @@ $res = $db->get(array('tbl_employees', 'tbl_employee_details'), array(
     'tbl_employee_details' => array(
          '*'
     )), array(
-        'tbl_employee_details.emp_details_id', '=', 'tbl_employees.emp_details_id'
+        'emp_details_id'
     ));
