c1f3a43e71bcc234e10b476eb9f2613b3e7976f1
xTOMATOx <xTOMATOx@users.noreply.github.com>
2020-12-11 07:24:47 +0800

    Update Department_model.php

--- Model/Department_model.php
+++ Model/Department_model.php
@@ -1,6 +1,24 @@
 <?php
 class Department_model
 {
-
-
+    function get_data()
+    {
+        $dept_obj = $admin->readInfo(
+            'department',
+            'department_id, department',
+            'tbl_department',
+            array('status','=','Active'));
+        $results = array();
+        //Extract array data from array object $dept_obj and store to respective columns
+        $array_dept    = array_column($dept_obj, 'department');
+        $array_id      = array_column($dept_obj, 'department_id');
+    
+        $x=0;
+        //Merge those two arrays
+        foreach($array_id as $id)
+        {
+            $results[$id] = $array_dept[$x];
+            $x++;
+        }
+    }
 }
