767572525a379c8a43e12c44a97061e1e1dd253a
xTOMATOx <xTOMATOx@users.noreply.github.com>
2020-12-14 22:56:22 +0800

    Update

--- Public/View/Admin/create_department.php
+++ Public/View/Admin/create_department.php
@@ -89,36 +89,6 @@
 <script>
    
 
-     $('.archive').on('click', function()
-     {
-        var formData = { 
-                'set_fields'        :   'status',
-                'set_values'        :   'Inactive' ,
-                'condition_field'   :   'department_id',
-                'operator'          :   '=',
-                'condition_value'   :   $(this).attr('id')
-            };
-            console.log(formData);
-        $.ajax({
-                type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-                url         : '../../../Controller/Handler/Department_handler.php', // the url where we want to POST
-                data        : formData, // our data object
-                dataType    : 'json', // what type of data do we expect back from the server
-                            encode          : true,
-                success     : function(data){console.log(data);},
-                error       : function(data){console.log(data);}
-            }).done(function(data) {
-
-                // log data to the console so we can see
-                console.log(data);
-                var res = (data['success'] === false ? data['errors'] : data['message']);
-                
-                $('#message').text(res);
-                $('#alertModal').modal('show');
-
-                // here we will handle errors and validation messages
-            });
-
-     });
+     
 </script>
 
